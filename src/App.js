574e758b9dec6e2f3f81d03c4ccde56146c4af85
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-15 13:22:20 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>React Store App</h1> 
+     <h1>React Store App Breaking Changes live updategit</h1> 
     </div>
   );
 }
