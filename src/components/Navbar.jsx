05b10f8bab3f216f22bac484ddcd80311f1a8583
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-18 22:31:46 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const container = styled.div`
+    height: 60px;
+    background-color: black;
+
+`
 
 const Navbar = () => {
   return (
