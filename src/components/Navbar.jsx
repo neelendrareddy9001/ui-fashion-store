a2854d9f870dde6e91950aebe0f7bcf8dbc0ac0f
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-18 22:40:44 +0200

    changes add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,27 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     height: 60px;
-    background-color: black;
+    background-color: white;
 
-`
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+
+`;
+
+const Left = styled.div``;
+const Center = styled.div``;
+const Right = styled.div``;
 
 const Navbar = () => {
   return (
     <Container>
-        Navbar
+        <Wrapper>
+            <Left>Left</Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
+        </Wrapper>
     </Container>
   )
 }
