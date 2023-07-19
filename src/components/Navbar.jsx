2b2c0cbe4d285316b3d0f3a848182da3394cb1b1
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-19 22:21:59 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -14,15 +15,41 @@ const Wrapper = styled.div`
 
 `;
 
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            <Left>Left</Left>
+            <Left>
+              <Language>EN</Language>
+              <SearchContainer>
+                input 
+                <Search/>
+              </SearchContainer>
+            </Left>
             <Center>Center</Center>
             <Right>Right</Right>
         </Wrapper>
