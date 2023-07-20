3639b2fc78fc697546ba1897017c35ece6b58e58
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-20 22:58:15 +0200

    add

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import styled from 'styled-components';
+
+//Arrow Mui icons
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+
+const Arrow = styled.div`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+
+  left : ${props => props.direction === "left" && "10px"};
+  right: ${props => props.direction === "right" && "10px"};
+`;
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <Container>
+        <Arrow direction="left" >
+            <ArrowLeftOutlinedIcon/>
+        </Arrow>
+        <Arrow direction="right" >
+            <ArrowRightOutlinedIcon/>
+        </Arrow>
+    </Container>
   )
 }
 
