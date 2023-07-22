8c309b891290ea89622fd40927b47526434fbdb4
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-22 10:21:47 +0200

    Add

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -23,9 +23,35 @@ const Arrow = styled.div`
   top: 0;
   bottom: 0;
   margin: auto;
-
   left : ${props => props.direction === "left" && "10px"};
   right: ${props => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ImgContainer = styled.div`
+  width: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+  flex: 1;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
 `;
 
 const Slider = () => {
@@ -34,6 +60,16 @@ const Slider = () => {
         <Arrow direction="left" >
             <ArrowLeftOutlinedIcon/>
         </Arrow>
+        <Wrapper>
+          <Slide>
+          <ImgContainer>
+              <ImgContainer>
+                  <Image src='https://i.ibb.co/cXFnLLV/3.png' />
+              </ImgContainer>
+              <InfoContainer></InfoContainer>
+          </ImgContainer>
+          </Slide>
+        </Wrapper>
         <Arrow direction="right" >
             <ArrowRightOutlinedIcon/>
         </Arrow>
