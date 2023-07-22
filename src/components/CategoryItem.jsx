678c8fcfbf7ee5ff40d543ae152ebd5ca9ea3dce
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-22 22:46:39 +0200

    Add

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,10 +4,13 @@ import  styled  from "styled-components";
 const Container = styled.div`
   flex: 1;
   margin: 3px;
+  height: 70vh;
 `;
 
 const Image = styled.img`
-  width: 100%
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 const Info = styled.div``;
