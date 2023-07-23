b801454f339857645b55f63bfd5f8c43c267aab9
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-23 10:33:20 +0200

    add

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,18 +8,43 @@ const Container = styled.div`
   background-color : #fcf5f5;
   display : flex;
   align-items : center;
-  justify-content : center; 
+  justify-content : center;
+  flex-direction: column; 
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
 
-const Description = styled.div``;
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
 
-const InputContainer = styled.div``;
+const InputContainer = styled.div`
+width: 50%;
+height: 40px;
+background-color: white;
+display : flex;
+justify-content: space-between;
+border: 1px solid lightgray;
+`;
 
-const Input = styled.div``;
+const Input = styled.input`
+  border : none;
+  flex : 8;
+  padding-left : 20px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  flex : 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  
+`;
 
 const Newsletter = () => {
   return (
