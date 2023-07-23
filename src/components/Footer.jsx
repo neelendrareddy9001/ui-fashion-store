7df950821c65e6c2343af732dacd9b8d0f9db08d
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-23 22:33:07 +0200

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,11 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import PinterestIcon from '@mui/icons-material/Pinterest';
 
+import PlaceIcon from '@mui/icons-material/Place';
+import CallIcon from '@mui/icons-material/Call';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
+
 
 const Container = styled.div`
   display : flex;
@@ -33,14 +38,48 @@ const SocialIcon = styled.h1`
   height: 40px;
   border-radius: 50%;
   color: white;
+  background-color: #${props => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 20px;
 `;
 
 const Center = styled.div`
   flex : 1;
+  padding: 20px;
+`;
+
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding : 0;
+  list-style : none;
+  display : flex;
+  flex-wrap : wrap;
+`;
+
+const ListItem = styled.li`
+  width : 50%;
+  margin-bottom : 10px;
 `;
 
 const Right = styled.div`
   flex : 1;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom : 20px;
+  display : flex;
+  align-items : center;
+`;
+
+const Payment = styled.img`
+  width : 50%;
 `;
 
 const Footer = () => {
@@ -54,22 +93,48 @@ const Footer = () => {
               humour, or randomised words which don’t look even slightly believable.
           </Desc>
           <SocialIconCenter>
-            <SocialIcon>
+            <SocialIcon color='3B5999'>
                 <FacebookIcon/>
             </SocialIcon>
-            <SocialIcon>
+            <SocialIcon color='E4405F'>
                 <InstagramIcon/>
             </SocialIcon>
-            <SocialIcon>
+            <SocialIcon color='55ACEE'>
                 <TwitterIcon/>
             </SocialIcon>
-            <SocialIcon>
+            <SocialIcon color='E60023'>
                 <PinterestIcon/>
             </SocialIcon>
           </SocialIconCenter>
         </Left>
-        <Center></Center>
-        <Right></Right>
+        <Center>
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Man Fashion</ListItem>
+            <ListItem>Woman Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Wishlit</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+        </Center>
+        <Right>
+          <Title>Contact</Title>
+          <ContactItem>
+            <PlaceIcon style={{marginRight: "10px"}}/>622 Dixi Path, South Tobinchester 98336
+          </ContactItem>
+          <ContactItem>
+            <CallIcon style={{marginRight: "10px"}}/>+1 234 56 789
+          </ContactItem>
+          <ContactItem>
+            <MailOutlineIcon style={{marginRight: "10px"}}/> contact@lama.dev
+          </ContactItem>
+          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+        </Right>
     </Container>
   )
 }
