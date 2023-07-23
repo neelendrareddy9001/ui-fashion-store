bc96f0324dd8b893caaea16fa1e0907d79e44db8
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-23 08:38:28 +0200

    add

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -5,6 +5,7 @@ const Container = styled.div`
   flex: 1;
   margin: 3px;
   height: 70vh;
+  position: relative;
 `;
 
 const Image = styled.img`
@@ -13,11 +14,32 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-const Info = styled.div``;
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  color: white;
+  margin-bottom : 20px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  border : none;
+  padding : 10px;
+  background-color : white;
+  color : greay;
+  cursor : pointer;
+  font-weight : 600;
+
+`;
 
 const CategoryItem = ({item}) => {
   return (
@@ -25,7 +47,7 @@ const CategoryItem = ({item}) => {
           <Image src={item.img}/>
           <Info>
             <Title>{item.title}</Title>
-          <Button>SHOP NOW</Button>
+            <Button>SHOP NOW</Button>
           </Info>
       </Container>
   )
