572f455461e2498f70d65a443689f056cc975b78
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-24 20:47:49 +0200

    add

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,10 @@ import Announcement from '../components/Announcement';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 
+//import Material Ui icons
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
 
 const Container = styled.div``;
 
@@ -41,6 +45,78 @@ const Price = styled.span`
 `;
 
 
+const FilterContainer = styled.div`
+  width : 50%;
+  margin: 30px 0px;
+  display : flex;
+  justify-content : space-between;
+`;
+
+const Filter = styled.div`
+  display : flex;
+  align-items : center;
+`;
+
+const FilterTitle = styled.span`
+  font-size : 20px;
+  font-weight : 200;
+  margin-right: 4px;
+`;
+
+const FilterColor = styled.div`
+  width : 20px;
+  height : 20px;
+  border-radius : 50%;
+  background-color : ${(props) => props.color};
+  margin : 0px 5px;
+  cursor : pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left : 10px;
+  padding : 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display : flex;
+  align-items : center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display : flex;
+  align-items : center;
+  font-weight : 700;
+`;
+const Amount = styled.span`
+  width : 30px;
+  height : 30px;
+  border-radius : 10px;
+  border : 1px solid teal;
+  display : flex;
+  align-items : center;
+  justify-content : center;
+  margin : 0pc 5px;
+  cursor : pointer;
+`;
+const Button = styled.button`
+  padding : 15px;
+  border : 2px solid teal;
+  background-color : white;
+  cursor : pointer;
+  font-weight : 700;
+  border-radius: 25px;
+
+  &:hover {
+    transition : all 0.3s ease;
+    background-color : teal;
+    color : white;
+  }
+`;
+
+
 const Product = () => {
   return (
     <Container>
@@ -60,6 +136,33 @@ const Product = () => {
                 condimentum ac, volutpat ornare.
               </Desc>
               <Price>$ 20</Price>
+              <FilterContainer>
+                <Filter>
+                  <FilterTitle>Color</FilterTitle>
+                  <FilterColor color="black"/>
+                  <FilterColor color="darkblue"/>
+                  <FilterColor color="gray"/>
+                </Filter>
+                <Filter>
+                  <FilterTitle>Size</FilterTitle>
+                  <FilterSize>
+                    <FilterSizeOption>XS</FilterSizeOption>
+                    <FilterSizeOption>S</FilterSizeOption>
+                    <FilterSizeOption>M</FilterSizeOption>
+                    <FilterSizeOption>L</FilterSizeOption>
+                    <FilterSizeOption>XL</FilterSizeOption>
+                    <FilterSizeOption>XS</FilterSizeOption>
+                  </FilterSize>
+                </Filter>
+              </FilterContainer>
+              <AddContainer>
+                <AmountContainer>
+                  <RemoveIcon />
+                  <Amount>1</Amount>
+                  <AddIcon />
+                </AmountContainer>
+                <Button>ADD TO CART</Button>
+              </AddContainer>
             </InfoContainer>
           </Wrapper>
       <Newsletter/>
