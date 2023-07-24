763517d77f5d6e055a289e67a03529acbc5414ff
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-24 23:25:10 +0200

    add

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 // import ProductList from './pages/ProductList';
 // import Product from './pages/Product';
 // import Register from './pages/Register';
-import Login from './pages/Login';
+// import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 
 function App() {
   return (
-    <Login/>
+    <Cart/>
   );
 }
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+
+const Container = styled.div``;
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <Container>
+      <Navbar />
+      <Announcement />
+      Cart
+      <Footer />
+    </Container>
   )
 }
 
