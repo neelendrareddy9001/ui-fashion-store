8efc394f3c12fe6f2ae36c3020afd3ad7ca6f707
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-25 14:38:38 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 // import Product from './pages/Product';
 // import Register from './pages/Register';
 // import Login from './pages/Login';
-import Cart from './pages/Cart';
+// import Cart from './pages/Cart';
 
 
 function App() {
